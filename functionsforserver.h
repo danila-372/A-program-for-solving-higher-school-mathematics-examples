4a5727a5e77970a817ab579d24a78ddd5cee2405
KolesnikovaIrina412 <ira.kolesnikova.0504@mail.ru>
2025-06-26 03:32:30 +0300

    Update functionsforserver.h

--- functionsforserver.h
+++ functionsforserver.h
@@ -1,6 +1,6 @@
 ///
 /// \file functionsforserver.h
-/// \brief Заголовочный файл класса обработки клиентских команд
+/// \brief Заголовочный файл класса для обработки команд сервера
 ///
 
 #ifndef FUNCTIONSFORSERVER_H
@@ -11,102 +11,181 @@
 #include "db.h"
 
 ///
-/// \brief Класс для обработки сетевых команд клиентов
-/// \details Обрабатывает команды через TCP-сокеты, взаимодействует с базой данных.
-/// Реализует логику регистрации, аутентификации, работы с задачами и статистикой.
+/// \brief Класс для обработки команд, получаемых сервером
+///
+/// Предоставляет функционал для аутентификации, работы с задачами,
+/// управления пользователями и статистикой через TCP-соединение
 ///
 class FunctionsForServer : public QObject
 {
     Q_OBJECT
-
 public:
-
     ///
     /// \brief Конструктор класса
-    /// \param parent Родительский объект Qt
+    /// \param parent Родительский объект в Qt-иерархии
     ///
     explicit FunctionsForServer(QObject *parent = nullptr);
 
     ///
-    /// \brief Анализирует и выполняет команду от клиента
-    /// \param socket Указатель на сокет подключения
-    /// \param command Строка команды в формате "<действие> [параметры]"
-    /// \param username Текущий пользователь (для проверки прав)
-    ///
-    /// \note Поддерживаемые команды описаны в реализации parse()
+    /// \brief Основной метод обработки входящих команд
+    /// \param socket Указатель на сокет клиента
+    /// \param command Полученная команда
+    /// \param username Имя пользователя (если аутентифицирован)
     ///
     void parse(QTcpSocket *socket, const QString &command, const QString &username);
 
 private:
-
-    /// \name Приватные методы обработки команд
-    /// \{
+    // Аутентификация и управление пользователями
 
     ///
     /// \brief Аутентификация пользователя
-    /// \param socket Сокет для ответа
-    /// \param username Логин пользователя
-    /// \param password Пароль пользователя
-    /// \note Обновляет время последнего входа при успешной аутентификации
+    /// \param socket Указатель на сокет клиента
+    /// \param username Имя пользователя
+    /// \param password Пароль
     ///
     void auth(QTcpSocket *socket, const QString &username, const QString &password);
 
     ///
     /// \brief Регистрация нового пользователя
+    /// \param socket Указатель на сокет клиента
+    /// \param username Имя пользователя
+    /// \param password Пароль
     /// \param isAdmin Флаг администратора (по умолчанию false)
-    /// \note Пароль хранится в виде SHA-256 хеша
     ///
     void reg(QTcpSocket *socket, const QString &username, const QString &password, bool isAdmin = false);
 
     ///
     /// \brief Удаление пользователя
-    /// \param currentUsername Инициатор операции
-    /// \note Требует прав администратора у currentUsername
+    /// \param socket Указатель на сокет клиента
+    /// \param username Имя пользователя для удаления
+    /// \param currentUsername Имя текущего пользователя (для проверки прав)
     ///
     void deleteUser(QTcpSocket *socket, const QString &username, const QString &currentUsername);
 
+    // Работа с задачами
+
     ///
-    /// \brief Проверка решения математической задачи
-    /// \param problem Название задачи
-    /// \param answer Ответ пользователя (пустой для запроса)
-    /// \note Обновляет статистику пользователя
+    /// \brief Решение математической задачи
+    /// \param socket Указатель на сокет клиента
+    /// \param problem Текст задачи
+    /// \param answer Ответ пользователя
+    /// \param username Имя пользователя
     ///
     void solveMathProblem(QTcpSocket *socket, const QString &problem, const QString &answer, const QString &username);
 
     ///
-    /// \brief Отправка списка доступных задач
+    /// \brief Получение списка всех задач
+    /// \param socket Указатель на сокет клиента
     ///
     void listProblems(QTcpSocket *socket);
 
     ///
     /// \brief Добавление новой задачи
-    /// \param currentUsername Инициатор операции
-    /// \note Требует прав администратора
+    /// \param socket Указатель на сокет клиента
+    /// \param problem Текст задачи
+    /// \param answer Ответ к задаче
+    /// \param currentUsername Имя текущего пользователя (для проверки прав)
     ///
     void addProblem(QTcpSocket *socket, const QString &problem, const QString &answer, const QString &currentUsername);
 
     ///
-    /// \brief Обновление ответа задачи
-    /// \param currentUsername Инициатор операции
-    /// \note Требует прав администратора
+    /// \brief Обновление ответа к задаче
+    /// \param socket Указатель на сокет клиента
+    /// \param problem Текст задачи
+    /// \param newAnswer Новый ответ
+    /// \param currentUsername Имя текущего пользователя (для проверки прав)
     ///
     void updateProblem(QTcpSocket *socket, const QString &problem, const QString &newAnswer, const QString &currentUsername);
 
+    // Работа со статистикой
+
     ///
-    /// \brief Просмотр статистики пользователей
-    /// \param currentUsername Инициатор запроса
-    /// \note Требует прав администратора
+    /// \brief Просмотр статистики пользователя
+    /// \param socket Указатель на сокет клиента
+    /// \param username Имя пользователя
+    /// \param currentUsername Имя текущего пользователя (для проверки прав)
     ///
     void viewUserStats(QTcpSocket *socket, const QString &username, const QString &currentUsername);
 
     ///
-    /// \brief Отображение общей статистики всех пользователей
-    /// \param currentUsername Инициатор запроса
-    /// \note Требует прав администратора. Формат:
-    /// "User: <логин> | Total: <задач> | Correct: <решено> | Last Login: <время>"
+    /// \brief Просмотр собственной статистики
+    /// \param socket Указатель на сокет клиента
+    /// \param username Имя пользователя
     ///
-    void showUserStats(QTcpSocket *socket, const QString &currentUsername);
-    /// \}
+    void viewMyStats(QTcpSocket *socket, const QString &username);
+
+    ///
+    /// \brief Просмотр статистики всех пользователей
+    /// \param socket Указатель на сокет клиента
+    /// \param currentUsername Имя текущего пользователя (для проверки прав)
+    ///
+    void viewAllStats(QTcpSocket* socket, const QString& currentUsername);
+
+    // Категории задач
+
+    ///
+    /// \brief Получение списка категорий задач
+    /// \param socket Указатель на сокет клиента
+    ///
+    void listCategories(QTcpSocket *socket);
+
+    ///
+    /// \brief Получение задач по категории
+    /// \param socket Указатель на сокет клиента
+    /// \param category Название категории
+    ///
+    void getCategoryProblems(QTcpSocket *socket, const QString &category);
+
+    // Администрирование
+
+    ///
+    /// \brief Повышение пользователя до администратора
+    /// \param socket Указатель на сокет клиента
+    /// \param targetUser Имя пользователя для повышения
+    /// \param adminUser Имя администратора, выполняющего операцию
+    ///
+    void promoteUser(QTcpSocket *socket, const QString &targetUser, const QString &adminUser);
+
+    ///
+    /// \brief Получение списка пользователей
+    /// \param socket Указатель на сокет клиента
+    /// \param currentUsername Имя текущего пользователя (для проверки прав)
+    ///
+    void listUsers(QTcpSocket *socket, const QString &currentUsername);
+
+    ///
+    /// \brief Генерация задач по категории
+    /// \param socket Указатель на сокет клиента
+    /// \param category Категория задач
+    /// \param currentUsername Имя текущего пользователя (для проверки прав)
+    ///
+    void generateProblems(QTcpSocket *socket, const QString &category, const QString &currentUsername);
+
+    // Внутренние методы
+
+    ///
+    /// \brief Обработка запроса на получение задач
+    /// \param socket Указатель на сокет клиента
+    /// \param category Категория задач
+    ///
+    void handleGetProblems(QTcpSocket* socket, const QString& category);
+
+    ///
+    /// \brief Проверка корректности задачи
+    /// \param problem Текст задачи
+    /// \return true, если задача корректна, false в противном случае
+    ///
+    bool isValidProblem(const QString& problem);
+
+    ///
+    /// \brief Обработка проверки ответа на задачу
+    /// \param socket Указатель на сокет клиента
+    /// \param category Категория задачи
+    /// \param problemIndex Индекс задачи
+    /// \param answer Ответ пользователя
+    /// \param username Имя пользователя
+    ///
+    void handleCheckAnswer(QTcpSocket* socket, const QString& category, int problemIndex, const QString& answer, const QString& username);
 };
 
 #endif // FUNCTIONSFORSERVER_H
