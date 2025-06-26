053e56b5e0c58ab16f9825fa131fbc3ab09a95f5
KolesnikovaIrina412 <ira.kolesnikova.0504@mail.ru>
2025-06-26 03:39:38 +0300

    Update mytcpserver.h

--- mytcpserver.h
+++ mytcpserver.h
@@ -1,6 +1,6 @@
 ///
 /// \file mytcpserver.h
-/// \brief Заголовочный файл класса TCP-сервера
+/// \brief Заголовочный файл класса TCP-сервера для математического сервиса
 ///
 
 #ifndef MYTCPSERVER_H
@@ -9,69 +9,101 @@
 #include <QObject>
 #include <QTcpServer>
 #include <QTcpSocket>
+#include <QSet>
+#include <QMap>
 #include "db.h"
-#include "functionsforserver.h"
 
- ///
- /// \brief Класс TCP-сервера для обработки сетевых подключений
- ///
- /// \details Обеспечивает:
- /// - Запуск сервера на порту 33333
- /// - Управление клиентскими подключениями
- /// - Маршрутизацию команд в FunctionsForServer
- /// - Отслеживание сессий пользователей
- ///
+///
+/// \brief Класс TCP-сервера для обработки математических запросов
+///
+/// Обеспечивает:
+/// - Управление подключениями клиентов
+/// - Обработку входящих команд
+/// - Взаимодействие с базой данных
+///
 class MyTcpServer : public QObject
 {
     Q_OBJECT
-
 public:
-
     ///
     /// \brief Конструктор сервера
-    /// \param parent Родительский объект Qt
-    /// \note Инициализирует сервер, БД и тестовые данные
+    /// \param parent Родительский объект
     ///
     explicit MyTcpServer(QObject *parent = nullptr);
 
     ///
     /// \brief Деструктор сервера
-    /// \note Корректно завершает работу сервера и закрывает соединения
     ///
     ~MyTcpServer();
 
+    ///
+    /// \brief Запуск сервера на указанном порту
+    /// \param port Номер порта для прослушивания
+    /// \return true в случае успешного запуска, false при ошибке
+    ///
+    bool start(quint16 port);
+
+    ///
+    /// \brief Остановка сервера
+    ///
+    void stop();
+
+    ///
+    /// \brief Установка максимального количества клиентов
+    /// \param max Максимальное число одновременных подключений
+    ///
+    void setMaxClients(int max);
+
+signals:
+    ///
+    /// \brief Сигнал о подключении нового клиента
+    /// \param descriptor Дескриптор сокета клиента
+    ///
+    void clientConnected(qintptr descriptor);
+
+    ///
+    /// \brief Сигнал об отключении клиента
+    /// \param descriptor Дескриптор сокета клиента
+    ///
+    void clientDisconnected(qintptr descriptor);
+
 private slots:
+    ///
+    /// \brief Слот обработки нового подключения
+    ///
+    void onNewConnection();
 
     ///
-    /// \brief Слот для обработки новых подключений
-    /// \details:
-    /// - Создает новый сокет
-    /// - Отправляет приветственное сообщение
-    /// - Настраивает обработчики данных и отключения
+    /// \brief Слот обработки отключения клиента
     ///
-    void slotNewConnection();
+    void onClientDisconnected();
 
     ///
-    /// \brief Слот для обработки входящих данных
-    /// \details:
-    /// - Читает команды от клиента
-    /// - Обрабатывает аутентификацию
-    /// - Перенаправляет команды в FunctionsForServer
+    /// \brief Слот обработки входящих данных
     ///
-    void slotServerRead();
-
-    ///
-    /// \brief Слот для обработки отключения клиента
-    /// \details:
-    /// - Удаляет сокет из списка активных подключений
-    /// - Освобождает ресурсы соединения
-    ///
-    void slotClientDisconnected();
+    void onReadyRead();
 
 private:
-    QTcpServer *mTcpServer;                 ///< Основной объект TCP-сервера ///
-    FunctionsForServer functions;           ///< Обработчик клиентских команд ///
-    QMap<QTcpSocket*, QString> clientLogins;    ///< Карта активных сессий: сокет -> логин ///
+    ///
+    /// \brief Обработка команды от клиента
+    /// \param client Указатель на сокет клиента
+    /// \param command Текст команды
+    ///
+    void processCommand(QTcpSocket* client, const QString& command);
+
+    ///
+    /// \brief Отправка ответа клиенту
+    /// \param client Указатель на сокет клиента
+    /// \param response Текст ответа
+    ///
+    void sendResponse(QTcpSocket* client, const QString& response);
+
+    QTcpServer* server;          ///< Основной объект TCP-сервера
+    QSet<QTcpSocket*> clients;   ///< Множество активных клиентских подключений
+    QMap<QTcpSocket*, QByteArray> clientBuffers; ///< Буферы данных для каждого клиента
+    DB& db;                      ///< Ссылка на объект базы данных
+    int maxClients;              ///< Максимальное количество клиентов
 };
 
 #endif // MYTCPSERVER_H
+
